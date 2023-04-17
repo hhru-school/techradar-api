929fe6c7f771f38fc93ed2a9917108f30360b77c
Dmitrii Daragan <dm.daragan1@yandex.ru>
2023-04-17 18:26:46 +0300

    Implemented RingService class

---
+++ src/main/java/ru/hh/techradar/service/RingService.java
@@ -0,0 +1,58 @@
+package ru.hh.techradar.service;
+
+import jakarta.transaction.Transactional;
+import java.time.Instant;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import ru.hh.techradar.entity.Ring;
+import ru.hh.techradar.exception.NotFoundException;
+import ru.hh.techradar.mapper.RingMapper;
+import ru.hh.techradar.repository.RingRepository;
+
+@Service
+public class RingService implements BaseService<Long, Ring> {
+
+  private final RingRepository ringRepository;
+  private final RingMapper ringMapper;
+
+  public RingService(RingRepository ringRepository, RingMapper ringMapper) {
+    this.ringRepository = ringRepository;
+    this.ringMapper = ringMapper;
+  }
+
+  @Override
+  @Transactional
+  public Ring findById(Long id) {
+    return ringRepository.findById(id).orElseThrow(() -> new NotFoundException(Ring.class, id));
+  }
+
+  @Override
+  @Transactional
+  public void deleteById(Long id) {
+    ringRepository.deleteById(id);
+  }
+
+  @Override
+  @Transactional
+  public Ring update(Long id, Ring entity) {
+    Ring found = ringRepository.findById(id).orElseThrow(() -> new NotFoundException(Ring.class, id));
+    return ringRepository.update(ringMapper.toUpdate(found, entity));
+  }
+
+  @Override
+  @Transactional
+  public Ring save(Ring entity) {
+    return ringRepository.save(entity);
+  }
+
+  @Override
+  @Transactional
+  public List<Ring> findAll() {
+    return ringRepository.findAll();
+  }
+
+  @Transactional
+  public List<Ring> fetchRingsByRadarId(Long radarId) {
+    return ringRepository.fetchRingsByRadarId(radarId).orElseThrow(() -> new NotFoundException(Ring.class, radarId));
+  }
+}
