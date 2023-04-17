4f57e5c9e6c01bdb116adc82e17663e018979539
Nedomets Alexandr <anedomets@yandex.ru>
2023-04-17 09:38:16 +0300

    add default batch size

--- src/main/java/ru/hh/techradar/config/HibernateConfig.java
+++ src/main/java/ru/hh/techradar/config/HibernateConfig.java
@@ -30,6 +30,8 @@ public class HibernateConfig {
   private String DIALECT;
   @Value("${db.hbm2ddl-auto}")
   private String HBM2DDL_AUTO;
+  @Value("${db.default-batch-fetch-size}")
+  private String DEFAULT_BATCH_FETCH_SIZE;
 
   @Bean
   public DataSource dataSource() {
@@ -51,6 +53,7 @@ public class HibernateConfig {
     properties.put(Environment.DIALECT, DIALECT);
     properties.put(Environment.SHOW_SQL, SHOW_SQL);
     properties.put(Environment.HBM2DDL_AUTO, HBM2DDL_AUTO);
+    properties.put(Environment.DEFAULT_BATCH_FETCH_SIZE, DEFAULT_BATCH_FETCH_SIZE);
 
     localSessionFactoryBean.setHibernateProperties(properties);
     return localSessionFactoryBean;
