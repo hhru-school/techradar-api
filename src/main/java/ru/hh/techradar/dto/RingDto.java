39586697f4098b50dcdb9e9b534d97bf07db7cd7
Dmitrii Daragan <dm.daragan1@yandex.ru>
2023-04-18 11:57:38 +0300

    Removed unused fields from RingDto

--- src/main/java/ru/hh/techradar/dto/RingDto.java
+++ src/main/java/ru/hh/techradar/dto/RingDto.java
@@ -1,28 +1,19 @@
 package ru.hh.techradar.dto;
 
-import java.time.Instant;
-import java.util.List;
 import java.util.Objects;
-import ru.hh.techradar.entity.Radar;
-import ru.hh.techradar.entity.RingSetting;
 
 public class RingDto {
   private Long id;
   private String name;
   private Integer position;
-  private Instant removedAt;
-  private Instant creationTime;
-  private Instant lastChangeTime;
 
-  public RingDto() {}
+  public RingDto() {
+  }
 
-  public RingDto(Long id, String name, Integer position, Instant creationTime, Instant lastChangeTime, Instant removedAt) {
+  public RingDto(Long id, String name, Integer position) {
     this.id = id;
-    this.removedAt = removedAt;
     this.name = name;
     this.position = position;
-    this.creationTime = creationTime;
-    this.lastChangeTime = lastChangeTime;
   }
 
   public Integer getPosition() {
@@ -49,30 +40,6 @@ public class RingDto {
     this.id = id;
   }
 
-  public Instant getRemovedAt() {
-    return removedAt;
-  }
-
-  public void setRemovedAt(Instant removedAt) {
-    this.removedAt = removedAt;
-  }
-
-  public Instant getCreationTime() {
-    return creationTime;
-  }
-
-  public void setCreationTime(Instant creationTime) {
-    this.creationTime = creationTime;
-  }
-
-  public Instant getLastChangeTime() {
-    return lastChangeTime;
-  }
-
-  public void setLastChangeTime(Instant lastChangeTime) {
-    this.lastChangeTime = lastChangeTime;
-  }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -82,18 +49,14 @@ public class RingDto {
       return false;
     }
     RingDto ringDto = (RingDto) o;
-    return Objects.equals(id, ringDto.id) && Objects.equals(name, ringDto.name) && Objects.equals(
-        position,
-        ringDto.position
-    ) && Objects.equals(removedAt, ringDto.removedAt) && Objects.equals(
-        creationTime,
-        ringDto.creationTime
-    ) && Objects.equals(lastChangeTime, ringDto.lastChangeTime);
+    return Objects.equals(id, ringDto.id)
+        && Objects.equals(name, ringDto.name)
+        && Objects.equals(position, ringDto.position);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name, position, removedAt, creationTime, lastChangeTime);
+    return Objects.hash(id, name, position);
   }
 
   @Override
@@ -102,9 +65,6 @@ public class RingDto {
         "id=" + id +
         ", name='" + name + '\'' +
         ", position=" + position +
-        ", removedAt=" + removedAt +
-        ", creationTime=" + creationTime +
-        ", lastChangeTime=" + lastChangeTime +
         '}';
   }
 }
