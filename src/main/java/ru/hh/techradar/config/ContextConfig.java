589ac05ceb90022b0c7f9d846c37f8e62b981f52
Nedomets Alexandr <anedomets@yandex.ru>
2023-04-28 20:14:03 +0300

    TR-7 remove PreRequestFilter and add ParamExceptionMapper

--- src/main/java/ru/hh/techradar/config/ContextConfig.java
+++ src/main/java/ru/hh/techradar/config/ContextConfig.java
@@ -4,12 +4,8 @@ import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.hh.techradar.controller.CompanyController;
-import ru.hh.techradar.controller.QuadrantController;
-import ru.hh.techradar.controller.RadarController;
-import ru.hh.techradar.controller.RingController;
-import ru.hh.techradar.exception.DateParseExceptionMapper;
 import ru.hh.techradar.exception.NotFoundExceptionMapper;
+import ru.hh.techradar.exception.ParamExceptionMapper;
 import ru.hh.techradar.util.DateFormatParamConverterProvider;
 
 @Configuration
@@ -21,9 +17,8 @@ public class ContextConfig {
     ResourceConfig resourceConfig = new ResourceConfig();
     resourceConfig.packages("ru.hh.techradar.controller");
     resourceConfig.register(NotFoundExceptionMapper.class);
-    resourceConfig.register(DateParseExceptionMapper.class);
+    resourceConfig.register(ParamExceptionMapper.class);
     resourceConfig.register(DateFormatParamConverterProvider.class);
-    resourceConfig.register(PreRequestFilter.class);
     return resourceConfig;
   }
 }
