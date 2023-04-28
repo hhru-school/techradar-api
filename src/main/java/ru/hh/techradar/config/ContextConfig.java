95e76ef847908e1ea8b30d857f7f72afb1554b9e
Nedomets Alexandr <anedomets@yandex.ru>
2023-04-29 02:33:17 +0300

    TR-6 add EntityExistsExceptionMapper

--- src/main/java/ru/hh/techradar/config/ContextConfig.java
+++ src/main/java/ru/hh/techradar/config/ContextConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.hh.techradar.controller.CompanyController;
 import ru.hh.techradar.controller.UserController;
+import ru.hh.techradar.exception.EntityExistsExceptionMapper;
 import ru.hh.techradar.exception.NotFoundExceptionMapper;
 
 @Configuration
@@ -16,6 +17,7 @@ public class ContextConfig {
     resourceConfig.register(CompanyController.class);
     resourceConfig.register(UserController.class);
     resourceConfig.register(NotFoundExceptionMapper.class);
+    resourceConfig.register(EntityExistsExceptionMapper.class);
     return resourceConfig;
   }
 }
