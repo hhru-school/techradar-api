d4b9fde7643159ef03e7f2ab29ea8cccff85a8c7
Nedomets Alexandr <anedomets@yandex.ru>
2023-04-29 00:34:10 +0300

    TR-7 add ConstraintViolationExceptionMapper and refactoring ContextConfig

--- src/main/java/ru/hh/techradar/config/ContextConfig.java
+++ src/main/java/ru/hh/techradar/config/ContextConfig.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.hh.techradar.exception.ConstraintViolationExceptionMapper;
 import ru.hh.techradar.exception.NotFoundExceptionMapper;
 import ru.hh.techradar.exception.ParamExceptionMapper;
 import ru.hh.techradar.util.DateFormatParamConverterProvider;
@@ -16,9 +17,18 @@ public class ContextConfig {
   public ResourceConfig resourceConfig() {
     ResourceConfig resourceConfig = new ResourceConfig();
     resourceConfig.packages("ru.hh.techradar.controller");
+    exceptionMapperConfig(resourceConfig);
+    converterProviderConfig(resourceConfig);
+    return resourceConfig;
+  }
+
+  public void exceptionMapperConfig(ResourceConfig resourceConfig) {
     resourceConfig.register(NotFoundExceptionMapper.class);
     resourceConfig.register(ParamExceptionMapper.class);
+    resourceConfig.register(ConstraintViolationExceptionMapper.class);
+  }
+
+  public void converterProviderConfig(ResourceConfig resourceConfig) {
     resourceConfig.register(DateFormatParamConverterProvider.class);
-    return resourceConfig;
   }
 }
